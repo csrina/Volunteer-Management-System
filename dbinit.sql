22be96e1384fb51161c7c68f0d3afdba0d0747c6
stephendoyle1992 <32746416+stephendoyle1992@users.noreply.github.com>
2018-01-24 20:01:38 -0700

    Added database creation and connection at top of file. Added clocking table. Minor tweeks to tables also included

--- dbinit.sql
+++ dbinit.sql
@@ -1,38 +1,52 @@
+create database caraway;
+
+\c caraway;
+
 create table room (
-    roomID serial PRIMARY KEY,
-    name text,
-    teacher text,
-    roomNum text
+    RoomID serial PRIMARY KEY,
+    Name text,
+    Teacher text,
+    RoomNum text
 );
 
 /* what else do we want in family? */
 create table family (
-    familyID serial PRIMARY KEY,
-    
+    FamilyID serial PRIMARY KEY,
+    SurName text
 );
 
-create table user (
-    userID serial PRIMARY KEY,
-    familyID serial REFERENCES family (familyID),
-    firstName text,
-    lastName text,
-    phoneNumber text, /* text for now, probably a better format available */
-)
+create table users (
+    UserID serial PRIMARY KEY,
+    Role int DEFAULT 1,
+    FamilyID serial REFERENCES family (familyID),
+    UserName text UNIQUE,
+    Password text, /* CURRENTLY PLAINTEXT!!! */
+    FirstName text,
+    LastName text,
+    PhoneNumber text /* text for now, probably a better format available */
+
+);
 
 create table block (
-    blockID serial PRIMARY KEY,
-    blockStart TIMESTAMP,
-    blockEnd TIMESTAMP,
-    room serial REFERENCES room(roomID),
-    modifier int DEFAULT 1,
-    note text
+    BlockID serial PRIMARY KEY,
+    BlockStart TIMESTAMP,
+    BlockEnd TIMESTAMP,
+    Room serial REFERENCES room(roomID),
+    Modifier int DEFAULT 1,
+    Note text
 );
 
 create table booking (
-    bookingID serial PRIMARY KEY,
-    blockID serial REFERENCES block (blockID),
-    familyID serial REFERENCES family (familyID),
-    bookingStart TIMESTAMP,
-    bookingEnd TIMESTAMP
+    BookingID serial PRIMARY KEY,
+    BlockID serial REFERENCES block (blockID),
+    BamilyID serial REFERENCES family (familyID),
+    BookingStart TIMESTAMP,
+    BookingEnd TIMESTAMP
+);
+
+create table clocking (
+    BookingID serial REFERENCES booking (BookingID),
+    ClockIn TIMESTAMP,
+    ClockOut TIMESTAMP
 );
 
