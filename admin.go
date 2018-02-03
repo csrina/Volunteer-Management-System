df405cb9d8c919a99b0a05e6014cc2952521d0bc
stephendoyle1992 <doyles8@mymacewan.ca>
2018-02-03 15:46:28 -0700

    Rebase the gross switch

--- admin.go
+++ admin.go
@@ -1,62 +1,65 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
 	"time"
 )
 
 func calSetup(w http.ResponseWriter, r *http.Request) {
-	//blocks have to be filled by either json decode or from forms
+	//blocave to be filled by either json decode or from forms
 	//start / end also need to be pulled from the request body
 	//blocks := []Timeblock{}
+	w.WriteHeader(http.StatusCreated)
 }
 
-func applyTemplate(startDate time.Time, stopDate time.Time, blocks []TimeBlock) {
+func setAndInsertBlock(t *sql.Tx, block TimeBlock, newDate time.Time) error {
+	block.setDay(newDate)
+	err := block.insertBlock()
+	if err != nil {
+		t.Rollback()
+		return err
+	}
+	return nil
+}
+
+func applyTemplate(startDate time.Time, stopDate time.Time, blocks []TimeBlock) error {
 	Tx, err := db.Begin()
 	if err != nil {
-		logger.Fatal("Could not start database transaction")
+		return err
 	}
 	for startDate.Before(stopDate) {
 		for _, block := range blocks {
 			switch block.Start.Weekday() {
 			case time.Monday:
 				tempDate := startDate.AddDate(0, 0, 0)
-				block.setDay(tempDate)
-				err := block.insertBlock()
-				if err != nil {
-					Tx.Rollback()
+				if err := setAndInsertBlock(Tx, block, tempDate); err != nil {
+					return err
 				}
 				break
 			case time.Tuesday:
 				tempDate := startDate.AddDate(0, 0, 1)
-				block.setDay(tempDate)
-				err := block.insertBlock()
-				if err != nil {
-					Tx.Rollback()
+				if err := setAndInsertBlock(Tx, block, tempDate); err != nil {
+					return err
 				}
 				break
 			case time.Wednesday:
 				tempDate := startDate.AddDate(0, 0, 2)
-				block.setDay(tempDate)
-				err := block.insertBlock()
-				if err != nil {
-					Tx.Rollback()
+				if err := setAndInsertBlock(Tx, block, tempDate); err != nil {
+					return err
 				}
 				break
 			case time.Thursday:
 				tempDate := startDate.AddDate(0, 0, 3)
 				block.setDay(tempDate)
-				err := block.insertBlock()
-				if err != nil {
-					Tx.Rollback()
+				if err := setAndInsertBlock(Tx, block, tempDate); err != nil {
+					return err
 				}
 				break
 			case time.Friday:
 				tempDate := startDate.AddDate(0, 0, 4)
-				block.setDay(tempDate)
-				err := block.insertBlock()
-				if err != nil {
-					Tx.Rollback()
+				if err := setAndInsertBlock(Tx, block, tempDate); err != nil {
+					return err
 				}
 				break
 			}
@@ -64,6 +67,7 @@ func applyTemplate(startDate time.Time, stopDate time.Time, blocks []TimeBlock) 
 		startDate = startDate.AddDate(0, 0, 7)
 	}
 	Tx.Commit()
+	return nil
 }
 
 func (tb *TimeBlock) setDay(startDate time.Time) {
