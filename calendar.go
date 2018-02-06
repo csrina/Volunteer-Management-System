9fcbc8c66c95ec20835a99f434f38426d2191971
Alex Crowder <crowderd@mymacewan.ca>
2018-02-06 16:27:05 -0700

    Add routes for static files + schedule view\ni.e. working calendar --> just need to setup data streams

--- calendar.go
+++ calendar.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 )
 
+/* Will eventually be needed */
 type Page struct {
 	Title string
 }
 
+/* Renders the calendar found in views/calendar.gohtml */
 func renderCalendar(w http.ResponseWriter, r *http.Request) {
 	p := &Page{Title: "F Schedule"}
 	t, _ := template.ParseFiles("views/calendar.gohtml")
--- router.go
+++ router.go
@@ -25,12 +25,16 @@ func logging(f http.HandlerFunc) http.HandlerFunc {
 
 func createRouter() (*mux.Router, error) {
 	r := mux.NewRouter()
+	r.StrictSlash(true)
+	// static file handling (put assets in views folder)
+	r.PathPrefix("/views/").Handler(http.StripPrefix("/views/", http.FileServer(http.Dir("./views/"))))
+
 	s := r.PathPrefix("/api/v1").Subrouter()
 	s.HandleFunc("/", logging(baseRoute))
 	s.HandleFunc("/login/", logging(loginHandler)).Methods("POST")
 	s.HandleFunc("/admin/calendar/setup/", calSetup).Methods("POST")
 	s.HandleFunc("/admin/calendar/setup/", undoSetup).Methods("DELETE")
-
+	s.HandleFunc("/schedule/", logging(renderCalendar)).Methods("GET")
 	return r, nil
 }
 
