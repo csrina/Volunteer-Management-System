dc23ee796576e4cd5278ed48725e5374b93a82de
Graham <alexanderg@mymacewan.ca>
2018-02-08 13:10:50 -0700

    fix timestamp mistakes, add booking data

--- data.sql
+++ data.sql
@@ -29,41 +29,43 @@ VALUES('2018-01-01 15:00:00', '2018-01-01 17:00:00', 1, 1, 'afternoon block!');
 --Adds time blocks room 1 (january 2)
 
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-02 08:00:00', '2018-01-01 11:00:00', 1, 1, 'morning block!');
+VALUES('2018-01-02 08:00:00', '2018-01-02 11:00:00', 1, 1, 'morning block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-02 12:00:00', '2018-01-01 14:00:00', 1, 1, 'noon block!');
+VALUES('2018-01-02 12:00:00', '2018-01-02 14:00:00', 1, 1, 'noon block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-01 15:00:00', '2018-01-01 17:00:00', 1, 1, 'afternoon block!');
+VALUES('2018-01-02 15:00:00', '2018-01-02 17:00:00', 1, 1, 'afternoon block!');
 
 --Adds time blocks room 1 (january 3)
 
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-03 08:00:00', '2018-01-01 11:00:00', 1, 1, 'morning block!');
+VALUES('2018-01-03 08:00:00', '2018-01-03 11:00:00', 1, 1, 'morning block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-03 12:00:00', '2018-01-01 14:00:00', 1, 1, 'noon block!');
+VALUES('2018-01-03 12:00:00', '2018-01-03 14:00:00', 1, 1, 'noon block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-01 15:00:00', '2018-01-01 17:00:00', 1, 1, 'afternoon block!');
+VALUES('2018-01-03 15:00:00', '2018-01-03 17:00:00', 1, 1, 'afternoon block!');
 
 --Adds time blocks room 1 (january 4)
 
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-04 08:00:00', '2018-01-01 11:00:00', 1, 1, 'morning block!');
+VALUES('2018-01-04 08:00:00', '2018-01-04 11:00:00', 1, 1, 'morning block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-04 12:00:00', '2018-01-01 14:00:00', 1, 1, 'noon block!');
+VALUES('2018-01-04 12:00:00', '2018-01-04 14:00:00', 1, 1, 'noon block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-01 15:00:00', '2018-01-01 17:00:00', 1, 1, 'afternoon block!');
+VALUES('2018-01-04 15:00:00', '2018-01-04 17:00:00', 1, 1, 'afternoon block!');
 
------------------------------------------------------------------------------
------------------------------------------------------------------------------
 
 --Adds time blocks room 1 (january 5)
 
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-05 08:00:00', '2018-01-01 11:00:00', 1, 1, 'morning block!');
+VALUES('2018-01-05 08:00:00', '2018-01-05 11:00:00', 1, 1, 'morning block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-05 12:00:00', '2018-01-01 14:00:00', 1, 1, 'noon block!');
+VALUES('2018-01-05 12:00:00', '2018-01-05 14:00:00', 1, 1, 'noon block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-05 15:00:00', '2018-01-01 17:00:00', 1, 1, 'afternoon block!');
+VALUES('2018-01-05 15:00:00', '2018-01-05 17:00:00', 1, 1, 'afternoon block!');
+
+
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
 
 --Adds time blocks room 2 (january 1)
 
@@ -77,38 +79,38 @@ VALUES('2018-01-01 15:00:00', '2018-01-01 17:00:00', 2, 1, 'afternoon block!');
 --Adds time blocks room 2 (january 2)
 
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-02 08:00:00', '2018-01-01 11:00:00', 2, 1, 'morning block!');
+VALUES('2018-01-02 08:00:00', '2018-01-02 11:00:00', 2, 1, 'morning block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-02 12:00:00', '2018-01-01 14:00:00', 2, 1, 'noon block!');
+VALUES('2018-01-02 12:00:00', '2018-01-02 14:00:00', 2, 1, 'noon block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-01 15:00:00', '2018-01-01 17:00:00', 2, 1, 'afternoon block!');
+VALUES('2018-01-02 15:00:00', '2018-01-02 17:00:00', 2, 1, 'afternoon block!');
 
 --Adds time blocks room 2 (january 3)
 
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-03 08:00:00', '2018-01-01 11:00:00', 2, 1, 'morning block!');
+VALUES('2018-01-03 08:00:00', '2018-01-03 11:00:00', 2, 1, 'morning block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-03 12:00:00', '2018-01-01 14:00:00', 2, 1, 'noon block!');
+VALUES('2018-01-03 12:00:00', '2018-01-03 14:00:00', 2, 1, 'noon block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-01 15:00:00', '2018-01-01 17:00:00', 2, 1, 'afternoon block!');
+VALUES('2018-01-03 15:00:00', '2018-01-03 17:00:00', 2, 1, 'afternoon block!');
 
 --Adds time blocks room 2 (january 4)
 
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-04 08:00:00', '2018-01-01 11:00:00', 2, 1, 'morning block!');
+VALUES('2018-01-04 08:00:00', '2018-01-04 11:00:00', 2, 1, 'morning block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-04 12:00:00', '2018-01-01 14:00:00', 2, 1, 'noon block!');
+VALUES('2018-01-04 12:00:00', '2018-01-04 14:00:00', 2, 1, 'noon block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-01 15:00:00', '2018-01-01 17:00:00', 2, 1, 'afternoon block!');
+VALUES('2018-01-04 15:00:00', '2018-01-04 17:00:00', 2, 1, 'afternoon block!');
 
 --Adds time blocks room 2 (january 5)
 
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-05 08:00:00', '2018-01-01 11:00:00', 2, 1, 'morning block!');
+VALUES('2018-01-05 08:00:00', '2018-01-05 11:00:00', 2, 1, 'morning block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-05 12:00:00', '2018-01-01 14:00:00', 2, 1, 'noon block!');
+VALUES('2018-01-05 12:00:00', '2018-01-05 14:00:00', 2, 1, 'noon block!');
 INSERT INTO time_block(block_start, block_end, room_id, modifier, note)
-VALUES('2018-01-05 15:00:00', '2018-01-01 17:00:00', 2, 1, 'afternoon block!');
+VALUES('2018-01-05 15:00:00', '2018-01-05 17:00:00', 2, 1, 'afternoon block!');
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
