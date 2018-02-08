e053c8b7d7edcdec1d576bc67a2cd2d08afcebf2
stephendoyle1992 <doyles8@mymacewan.ca>
2018-02-07 17:23:09 -0700

    add bookingBlock + methods method to insert / update / delete blocks in DB

---
+++ booking.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"errors"
+	"time"
+)
+
+type bookingBlock struct {
+	BookingID int       `db:"booking_id" json:"bookingId"`
+	BlockID   int       `db:"block_id" json:"blockId"`
+	FamilyID  int       `db:"family_id" json:"familyId"`
+	UserID    int       `db:"user_id" json:"userID"`
+	Start     time.Time `db:"booking_start" json:"start"`
+	End       time.Time `db:"booking_end" json:"end"`
+}
+
+func (b *bookingBlock) insertBooking() error {
+	q := `INSERT INTO booking (block_id, family_id, user_id, 
+			booking_start, booking_end) 
+			VALUES ($1, $2, $3, $4, $5)
+			RETURNING booking_id`
+
+	err := db.QueryRow(q, b.BlockID, b.FamilyID, b.UserID, b.Start, b.End).Scan(&b.BookingID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *bookingBlock) updateBooking() error {
+	q := `UPDATE booking SET booking_start = $2, booking_end = $3
+			WHERE booking_id = $3`
+
+	results, err := db.Exec(q, b.BlockID, b.Start, b.End)
+	if err != nil {
+		return err
+	}
+	count, err := results.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if count != 1 {
+		err = errors.New("Booking not updated")
+		return err
+	}
+	return nil
+}
+
+func (b *bookingBlock) deleteBooking() error {
+	q := `DELETE FROM booking WHERE bookind_id = $1`
+
+	results, err := db.Exec(q, b.BlockID, b.Start, b.End)
+	if err != nil {
+		return err
+	}
+	count, err := results.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if count != 1 {
+		err = errors.New("Booking not deleted")
+		return err
+	}
+	return nil
+}
