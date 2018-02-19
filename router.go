5810726d5fdbdf0a1cc8092ce6c6b3a4c6befd41
csrina <virinamekhail@gmail.com>
2018-02-19 09:56:21 -0700

    Add separation between api routes and app routes

--- router.go
+++ router.go
@@ -15,7 +15,6 @@ func logging(f http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		logger.Println(r.URL.Path)
-
 		f.ServeHTTP(w, r)
 
 	})
@@ -36,39 +35,39 @@ func createRouter() (*mux.Router, error) {
 	r := mux.NewRouter()
 	r.Use(logging)
 	r.Use(errorMessage)
+
 	r.StrictSlash(true)
 	// static file handling (put assets in views folder)
 	r.PathPrefix("/views/").Handler(http.StripPrefix("/views/", http.FileServer(http.Dir("./views/"))))
-	// r.PathPrefix("/tmp/").Handler(http.StripPrefix("/tmp/", http.FileServer(http.Dir("./public/"))))
 
-	r.HandleFunc("/dashboard", loadDashboard)
-	r.HandleFunc("/calendar", loadCalendar)
+	// api routes+calls set up
+	apiRoutes(r)
 
+	// load login pages html tmplts
 	r.HandleFunc("/login", loadMainLogin)
 	l := r.PathPrefix("/login").Subrouter()
 	l.HandleFunc("/facilitator", loadLogin)
 	l.HandleFunc("/teacher", loadLogin)
 	l.HandleFunc("/admin", loadLogin)
 
-	s := r.PathPrefix("/api/v1").Subrouter()
+	//load dashboard and calendar pages
+	r.HandleFunc("/dashboard", loadDashboard)
+	r.HandleFunc("/calendar", loadCalendar)
 
+	return r, nil
+}
+
+func apiRoutes(r *mux.Router) {
+	s := r.PathPrefix("/api/v1").Subrouter()
 	s.HandleFunc("/admin/calendar/setup/", calSetup).Methods("POST")
 	s.HandleFunc("/admin/calendar/setup/", undoSetup).Methods("DELETE")
 	/* Events JSON routes for scheduler system */
 	s.HandleFunc("/events", getEvents).Methods("GET")
 	s.HandleFunc("/events/{target}", eventPostHandler).Methods("POST")
-	v := r.PathPrefix("/app").Subrouter()
-	// need redirect for '/' -> '/dashboard'
-	v.HandleFunc("/dashboard/", renderDashboard).Methods("GET")
-	/* Calendar requests */
-	v.HandleFunc("/schedule/", renderCalendar).Methods("GET")
-
-	l = s.PathPrefix("/login").Subrouter()
+	l := s.PathPrefix("/login").Subrouter()
 	l.HandleFunc("/facilitator/", loginHandler).Methods("POST")
 	l.HandleFunc("/teacher/", loginHandler).Methods("POST")
 	l.HandleFunc("/admin/", loginHandler).Methods("POST")
-
-	return r, nil
 }
 
 func baseRoute(w http.ResponseWriter, r *http.Request) {
