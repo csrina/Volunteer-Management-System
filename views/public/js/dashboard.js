1c2fb77c356171b8399a8214523ca600446551fc
Graham <alexanderg@mymacewan.ca>
2018-02-21 13:41:02 -0700

    update display of times on dashboard

--- views/public/js/dashboard.js
+++ views/public/js/dashboard.js
@@ -11,14 +11,24 @@ function input(data) {
     let done = document.getElementById("hoursDone");
     let booked = document.getElementById("hoursBooked");
     let table = document.getElementById("events");
-
+    let time = 0;
     for (let i = 0; i < data.length; i++) {
 	let row = document.createElement("tr");
+	let start = new Date(data[i].BlockStart);	
+	let startMonth = start.getMonth();
+	let startDate = start.getDate();
+	let startHours = start.getHours();
+	let startMinutes = start.getMinutes();
+	let end = new Date(data[i].BlockEnd);
+	let endMonth = end.getMonth();
+	let endDate = end.getDate();
+	let endHours = end.getHours();
+	let endMinutes = end.getMinutes();
 	row.innerHTML = `
-	    <td>${data[i].BlockStart} </td>
-	    <td> ${data[i].BlockEnd}</td>`
+	    <td style="border: solid">${startMonth}, ${startDate} at ${startHours}:${startMinutes}</td>
+	    <td style="border: solid">${endMonth}, ${endDate}</td>`
+	time += Math.abs(new Date(data[i].BlockEnd)- new Date(data[i].BlockStart))
 	table.appendChild(row);
     }
 }
-console.log("hi")
 load();
