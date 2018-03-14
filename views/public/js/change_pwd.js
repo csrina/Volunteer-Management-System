0a452a7bd1abdb4407ddd891dd1c531926690dbe
csrina <virinamekhail@gmail.com>
2018-03-14 17:28:34 +0000

    Refactoring done, still a bit of an issue in the UI after password has been updated

--- views/public/js/change_pwd.js
+++ views/public/js/change_pwd.js
@@ -11,7 +11,14 @@ function enterPressed(e) {
     }
 }
 
-function check() {
+function setAlertText(txt) {
+    var alrt = document.getElementById('alert_box');
+    alrt.innerHTML = txt;
+}
+
+function updateStatus(disable, neutral, isFailedPassword, isFailedReq) {
+    // status == 1 = disable fields
+    // status == 0 = enable fields
     var old_pwd_txt = document.getElementById('old_pwd');
     var old_pwd_lbl = document.getElementById('old_pwd_lbl');
     var old_message = document.getElementById('old_message');
@@ -24,122 +31,55 @@ function check() {
     var conf_pwd_lbl = document.getElementById('confirm_pwd_lbl');
     var conf_message = document.getElementById('conf_message');
 
+    var l = document.getElementById('alert_box');
     var submit_btn = document.getElementById('change_pwd_btn');
 
-    if (new_pwd_txt.value.length < 8) {
-        new_message.classList.remove("d-none");
-        new_pwd_lbl.classList.add("text-danger");
-        new_pwd_lbl.classList.remove("text-success");
-        new_pwd_txt.classList.add("is-invalid");
-        new_pwd_txt.classList.remove("is-valid");
+    if (disable) {
+        old_pwd_txt.disabled = true;
+        new_pwd_txt.disabled = true;
+        conf_pwd_txt.disabled = true;
+        submit_btn.disabled = true;
     }
     else {
-        new_message.classList.add("d-none");
-        new_pwd_lbl.classList.remove("text-danger");
-        new_pwd_lbl.classList.add("text-success");
-        new_pwd_txt.classList.remove("is-invalid");
-        new_pwd_txt.classList.add("is-valid");
+        old_pwd_txt.disabled = false;
+        new_pwd_txt.disabled = false;
+        conf_pwd_txt.disabled = false;
+        submit_btn.disabled = false;
     }
 
+    if (neutral) {
+        updateDisplay(old_pwd_lbl, old_pwd_txt, old_message, 0);
+        updateDisplay(new_pwd_lbl, new_pwd_txt, new_message, 0);
+        updateDisplay(conf_pwd_lbl, conf_pwd_txt, conf_message, 0);
+    }
 
-
-    if (new_pwd_txt.value == conf_pwd_txt.value && new_pwd_txt.value.length != 0 && conf_pwd_txt.value.length != 0) {
-        conf_message.classList.add("d-none");
-        conf_pwd_lbl.classList.remove("text-danger");
-        conf_pwd_lbl.classList.add("text-success");
-        conf_pwd_txt.classList.remove("is-invalid");
-        conf_pwd_txt.classList.add("is-valid");
-
-    }
-    else if (new_pwd_txt.value != conf_pwd_txt.value && conf_pwd_txt.value.length > 0) {
-        conf_message.innerText = "Needs to match new password";
-        conf_message.classList.remove("d-none");
-        conf_pwd_lbl.classList.add("text-danger");
-        conf_pwd_lbl.classList.remove("text-success");
-        conf_pwd_txt.classList.add("is-invalid");
-        conf_pwd_txt.classList.remove("is-valid");
-    }
-    else if (new_pwd_txt.value != conf_pwd_txt.value && conf_pwd_txt.value.length == 0) {
-        conf_message.innerText = "Cannot be empty and needs to match new password";
-        conf_message.classList.remove("d-none");
-        conf_pwd_lbl.classList.add("text-danger");
-        conf_pwd_lbl.classList.remove("text-success");
-        conf_pwd_txt.classList.add("is-invalid");
-        conf_pwd_txt.classList.remove("is-valid");
-    }
-    else if (new_pwd_txt.value.length == 0 && conf_pwd_txt.value.length == 0) {
-        conf_message.innerText = "Cannot be empty";
-        conf_message.classList.remove("d-none");
-        conf_pwd_lbl.classList.add("text-danger");
-        conf_pwd_lbl.classList.remove("text-success");
-        conf_pwd_txt.classList.add("is-invalid");
-        conf_pwd_txt.classList.remove("is-valid");
-    }
-    if (old_pwd_txt.value.length > 0) {
-        old_message.classList.add("d-none");
-        old_pwd_lbl.classList.remove("text-danger");
-        old_pwd_txt.classList.remove("is-invalid");
-
-    }
-    else {
-        old_message.classList.remove("d-none");
+    if (isFailedPassword) {
         old_pwd_lbl.classList.add("text-danger");
         old_pwd_txt.classList.add("is-invalid");
-
     }
 
-    if (old_pwd_txt.value.length == 0 || conf_pwd_txt.value.length == 0 ||
-        (new_pwd_txt.value != conf_pwd_txt.value && conf_pwd_txt.value.length > 0) ||
-        new_pwd_txt.value.length < 8) {
-        submit_btn.disabled = true;
-        return false;
+    if (isFailedReq) {
+        l.classList.remove("d-none");
     }
     else {
-        submit_btn.disabled = false;
-        return true;
+        l.classList.add("d-none");
     }
 
 
 }
 
-function change_pwd() {
+function eraseTextFields() {
     var old_pwd_txt = document.getElementById('old_pwd');
-    var old_pwd_lbl = document.getElementById('old_pwd_lbl');
-
     var new_pwd_txt = document.getElementById('new_pwd');
-    var new_pwd_lbl = document.getElementById('new_pwd_lbl');
-
-
     var conf_pwd_txt = document.getElementById('confirm_pwd');
-    var conf_pwd_lbl = document.getElementById('confirm_pwd_lbl');
+    old_pwd_txt.value = "";
+    new_pwd_txt.value = "";
+    conf_pwd_txt.value = "";
+}
 
-    var submit_btn = document.getElementById('change_pwd_btn');
+function showToaster(type, msg) {
 
-
-    old_pwd_lbl.classList.remove("text-danger");
-    old_pwd_txt.classList.remove("is-invalid");
-
-    old_pwd_txt.disabled = true;
-    new_pwd_txt.disabled = true;
-    conf_pwd_txt.disabled = true;
-    submit_btn.disabled = true;
-
-    var ready = check();
-    if (ready == false) {
-        old_pwd_txt.disabled = false;
-        new_pwd_txt.disabled = false;
-        conf_pwd_txt.disabled = false;
-        submit_btn.disabled = false;
-        return;
-    }
-
-    var location = document.getElementsByClassName('container');
-    var progress = location[0];
-    progress.insertAdjacentHTML('afterbegin',
-        '<div class="progress" id="progress_div" style="margin-bottom: 20px"> <div id="progress" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 25%"></div><br></div>')
-
-
-    Command: toastr["info"]("Updating Password ......")
+    Command: toastr[type](msg);
 
     toastr.options = {
         "closeButton": false,
@@ -157,78 +97,164 @@ function change_pwd() {
         "hideEasing": "linear",
         "showMethod": "fadeIn",
         "hideMethod": "fadeOut"
+    };
+}
+
+function addProgressBar() {
+    var location = document.getElementsByClassName('container');
+    var progress = location[0];
+    progress.insertAdjacentHTML('afterbegin',
+        '<div class="progress" id="progress_div" style="margin-bottom: 20px"> <div id="progress" class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 25%"></div><br></div>')
+}
+
+function updateProgress(precent) {
+    var p = document.getElementById('progress')
+    p.style.width = precent;
+}
+
+function removeProgress() {
+    var elem = document.getElementById('progress_div');
+    elem.parentNode.removeChild(elem);
+}
+
+function updateDisplay(lbl, txt, msg, state) {
+    var submit_btn = document.getElementById('change_pwd_btn');
+    // state == 0 neutral
+    // state == 1 pass/green
+    // state == 2 fail/red
+    if (state == 2) {
+        msg.classList.remove("d-none");
+        lbl.classList.add("text-danger");
+        lbl.classList.remove("text-success");
+        txt.classList.add("is-invalid");
+        txt.classList.remove("is-valid");
+        submit_btn.disabled = true;
+    }
+    if (state == 1) {
+        msg.classList.add("d-none");
+        lbl.classList.remove("text-danger");
+        lbl.classList.add("text-success");
+        txt.classList.remove("is-invalid");
+        txt.classList.add("is-valid");
+        submit_btn.disabled = false;
+    }
+    if (state == 0) {
+        msg.classList.add("d-none");
+        lbl.classList.remove("text-danger");
+        lbl.classList.remove("text-success");
+        txt.classList.remove("is-invalid");
+        txt.classList.remove("is-valid");
+        submit_btn.disabled = false;
+    }
+}
+
+function check() {
+    var old_pwd_txt = document.getElementById('old_pwd');
+    var old_pwd_lbl = document.getElementById('old_pwd_lbl');
+    var old_message = document.getElementById('old_message');
+
+    var new_pwd_txt = document.getElementById('new_pwd');
+    var new_pwd_lbl = document.getElementById('new_pwd_lbl');
+    var new_message = document.getElementById('new_message');
+
+    var conf_pwd_txt = document.getElementById('confirm_pwd');
+    var conf_pwd_lbl = document.getElementById('confirm_pwd_lbl');
+    var conf_message = document.getElementById('conf_message');
+
+    // state == 0 neutral
+    // state == 1 pass/green
+    // state == 2 fail/red
+
+    // tests new password length
+    if (new_pwd_txt.value.length < 8) {
+        updateDisplay(new_pwd_lbl, new_pwd_txt, new_message, 2);
+    }
+    else {
+        updateDisplay(new_pwd_lbl, new_pwd_txt, new_message, 1);
+
     }
 
+    // --------------------------------- tests new password
+    if (new_pwd_txt.value == conf_pwd_txt.value &&
+        new_pwd_txt.value.length != 0 &&
+        conf_pwd_txt.value.length != 0) {
+
+        updateDisplay(conf_pwd_lbl, conf_pwd_txt, conf_message, 1);
+    }
+    else if (new_pwd_txt.value != conf_pwd_txt.value &&
+        conf_pwd_txt.value.length > 0) {
+
+        conf_message.innerText = "Needs to match new password";
+        updateDisplay(conf_pwd_lbl, conf_pwd_txt, conf_message, 2);
+
+    }
+    else if (new_pwd_txt.value != conf_pwd_txt.value &&
+        conf_pwd_txt.value.length == 0) {
+
+        conf_message.innerText = "Cannot be empty and needs to match new password";
+        updateDisplay(conf_pwd_lbl, conf_pwd_txt, conf_message, 2);
+
+    }
+    else if (new_pwd_txt.value.length == 0 &&
+        conf_pwd_txt.value.length == 0) {
+
+        conf_message.innerText = "Cannot be empty";
+        updateDisplay(conf_pwd_lbl, conf_pwd_txt, conf_message, 2);
+
+    }
+
+    // --------------------------------- test old password
+    if (old_pwd_txt.value.length > 0) {
+        updateDisplay(old_pwd_lbl, old_pwd_txt, old_message, 0);
+
+    }
+    else {
+        updateDisplay(old_pwd_lbl, old_pwd_txt, old_message, 2);
+    }
+
+
+    if (old_pwd_txt.value.length == 0 || conf_pwd_txt.value.length == 0 ||
+        (new_pwd_txt.value != conf_pwd_txt.value && conf_pwd_txt.value.length > 0) ||
+        new_pwd_txt.value.length < 8) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function change_pwd() {
+    //disable fields, set fields to neutral, isFailedPassword, is FailedReg 
+    updateStatus(true, true, false, false);
+
+    var ready = check();
+    if (ready == false) {
+        updateStatus(false, false, false, false);
+        return;
+    }
+    addProgressBar();
+    showToaster("info", "Updating Password ......");
+
     // check old password
+    var old = document.getElementById('old_pwd').value;
     const api_call = "/api/v1/passwords";
-    console.log("one");
     const xmlhttp = new XMLHttpRequest(); // new HttpRequest instance
-    console.log("two");
     xmlhttp.onreadystatechange = function() {
         check();
-        console.log("STUFSSSS");
-        if (xmlhttp.readyState !== 4) {
-            console.log("not ready");
-            return;
-        }
+        if (xmlhttp.readyState !== 4) { return; }
         if (xmlhttp.status !== 202) {
             console.log("Failed to auth");
             console.log(xmlhttp.status);
             console.log(xmlhttp.responseText);
-            Command: toastr["error"]("Updating password failed.")
-
-            toastr.options = {
-                "closeButton": false,
-                "debug": false,
-                "newestOnTop": true,
-                "progressBar": false,
-                "positionClass": "toast-top-full-width",
-                "preventDuplicates": true,
-                "onclick": null,
-                "showDuration": "300",
-                "hideDuration": "1000",
-                "timeOut": "5000",
-                "extendedTimeOut": "1000",
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
-            }
-            var elem = document.getElementById('progress_div');
-            elem.parentNode.removeChild(elem);
-
-            var l = document.getElementById('alert_box');
-            l.classList.remove("d-none");
-
-            old_pwd_lbl.classList.add("text-danger");
-            old_pwd_txt.classList.add("is-invalid");
-
-            new_pwd_lbl.classList.remove("text-danger");
-            new_pwd_lbl.classList.remove("text-success");
-            new_pwd_txt.classList.remove("is-invalid");
-            new_pwd_txt.classList.remove("is-valid");
-
-            conf_pwd_lbl.classList.remove("text-danger");
-            conf_pwd_lbl.classList.remove("text-success");
-            conf_pwd_txt.classList.remove("is-invalid");
-            conf_pwd_txt.classList.remove("is-valid");
-
-            old_pwd_txt.disabled = false;
-            new_pwd_txt.disabled = false;
-            conf_pwd_txt.disabled = false;
-            submit_btn.disabled = false;
+            showToaster("error", "Updating password failed.");
+            removeProgress();
+            setAlertText('<strong>Warning!</strong> Password update failed, could not authenticate user. Please enter correct password.')
+            updateStatus(false, false, true, true);
             return;
         }
         else {
-            old_pwd_txt.disabled = true;
-            new_pwd_txt.disabled = true;
-            conf_pwd_txt.disabled = true;
-            submit_btn.disabled = true;
-            var l = document.getElementById('alert_box');
-            l.classList.add("d-none");
-            l.innerHTML = '<strong>Warning!</strong> Password update failed, could not authenticate user. Please enter correct password.'
-            var p = document.getElementById('progress')
-            p.style.width = '60%';
+            updateStatus(false, false, false, false);
+            updateProgress('60%');
             console.log("User authenticated");
             alert("Old Password Passed, next up updating password");
         }
@@ -237,123 +263,45 @@ function change_pwd() {
     xmlhttp.setRequestHeader("Content-Type", "application/json");
 
     const data = [];
-    for (let i = 0; i < old_pwd_txt.value.length; i++) {
-        data.push(old_pwd_txt.value.charCodeAt(i));
+    for (let i = 0; i < old.length; i++) {
+        data.push(old.charCodeAt(i));
     }
 
     xmlhttp.send(JSON.stringify({ password: data }));
-    console.log("one");
+
     // update new password
+    var newp = document.getElementById('new_pwd').value;
     const xmlhttp2 = new XMLHttpRequest(); // new HttpRequest instance
-    console.log("two");
     xmlhttp2.onreadystatechange = function() {
         check();
-        console.log("STUFSSSS");
-        if (xmlhttp2.readyState !== 4) {
-            console.log("not ready");
-            return;
-        }
+        if (xmlhttp2.readyState !== 4) { return; }
         if (xmlhttp2.status !== 200) {
             console.log("Failed to update Password");
             console.log(xmlhttp2.status);
             console.log(xmlhttp2.responseText);
-            Command: toastr["error"]("Updating password failed.")
-
-            toastr.options = {
-                "closeButton": false,
-                "debug": false,
-                "newestOnTop": true,
-                "progressBar": false,
-                "positionClass": "toast-top-full-width",
-                "preventDuplicates": true,
-                "onclick": null,
-                "showDuration": "300",
-                "hideDuration": "1000",
-                "timeOut": "5000",
-                "extendedTimeOut": "1000",
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
-            }
-            var elem = document.getElementById('progress_div');
-            elem.parentNode.removeChild(elem);
-
-            var l = document.getElementById('alert_box');
-            l.innerHTML = '<strong>Warning!</strong> Password update failed, could not update password. Please try again.'
-            l.classList.remove("d-none");
-
-            old_pwd_lbl.classList.remove("text-danger");
-            old_pwd_lbl.classList.remove("text-success");
-            old_pwd_txt.classList.remove("is-invalid");
-            old_pwd_txt.classList.remove("is-valid");
-
-
-            new_pwd_lbl.classList.remove("text-danger");
-            new_pwd_lbl.classList.remove("text-success");
-            new_pwd_txt.classList.remove("is-invalid");
-            new_pwd_txt.classList.remove("is-valid");
-
-            conf_pwd_lbl.classList.remove("text-danger");
-            conf_pwd_lbl.classList.remove("text-success");
-            conf_pwd_txt.classList.remove("is-invalid");
-            conf_pwd_txt.classList.remove("is-valid");
-
-            old_pwd_txt.disabled = false;
-            new_pwd_txt.disabled = false;
-            conf_pwd_txt.disabled = false;
-            submit_btn.disabled = false;
+            showToaster("error", "Updating password failed.");
+            removeProgress();
+            setAlertText('<strong>Warning!</strong> Password update failed, could not update password. Please try again.')
+            updateStatus(false, true, false, true);
             return;
         }
         else {
-            Command: toastr["success"]("Password Updated successfully ")
-
-            toastr.options = {
-                "closeButton": true,
-                "debug": false,
-                "newestOnTop": true,
-                "progressBar": false,
-                "positionClass": "toast-top-full-width",
-                "preventDuplicates": false,
-                "onclick": null,
-                "showDuration": "300",
-                "hideDuration": "1000",
-                "timeOut": "5000",
-                "extendedTimeOut": "1000",
-                "showEasing": "swing",
-                "hideEasing": "linear",
-                "showMethod": "fadeIn",
-                "hideMethod": "fadeOut"
-            }
-
-
-            old_pwd_txt.disabled = false;
-            old_pwd_txt.innerHTML = "";
-            new_pwd_txt.disabled = false;
-            new_pwd_txt.innerHTML = "";
-            conf_pwd_txt.disabled = false;
-            conf_pwd_txt.innerHTML = "";
-            submit_btn.disabled = false;
-            var l = document.getElementById('alert_box');
-            l.classList.add("d-none");
-            var p = document.getElementById('progress')
-            p.style.width = '100%';
+            showToaster("success", "Password Updated successfully ");
+            updateStatus(false, true, false, false);
+            updateProgress('100%');
             console.log("New Password set");
             alert("Password updated");
+            eraseTextFields();
+            removeProgress();
         }
     };
-    console.log("there");
     xmlhttp2.open("PUT", api_call, true);
-    console.log("four");
     xmlhttp2.setRequestHeader("Content-Type", "application/json");
-    console.log("five");
     var data2 = [];
-    console.log("six");
-    for (let i = 0; i < new_pwd_txt.value.length; i++) {
-        data2.push(new_pwd_txt.value.charCodeAt(i));
+    for (let i = 0; i < newp.length; i++) {
+        data2.push(newp.charCodeAt(i));
     }
     console.log(data2);
 
     xmlhttp2.send(JSON.stringify({ password: data2 }));
-    console.log("eight");
 }
